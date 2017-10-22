5e18f225e0460d312ac8badb2f80cb737ffb1828
Justas <glodjus@gmail.com>
2017-10-22 16:18:50 +0300

    Adding ScreenStateThread which takes screenshots every frame and stores them in a variable

---
+++ MouseRobot/src/Graphics/ScreenStateThread.cs
@@ -0,0 +1,160 @@
+﻿using Robot.Utils;
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+
+namespace Robot.Graphics
+{
+    public class ScreenStateThread
+    {
+        private bool m_Run, m_Init;
+
+        public object ScreenBmpLock = new object();
+        /// <summary>
+        /// Lock before using it, since data is constantly being written to this bitmap
+        /// </summary>
+        public Bitmap ScreenBmp
+        {
+            get
+            {
+                return m_ScreenBmp;
+            }
+            private set
+            {
+                m_ScreenBmp = value;
+            }
+        }
+        private Bitmap m_ScreenBmp;
+        private Bitmap m_TempBitmap;
+
+        private Device m_Device;
+        private Output1 m_Output1;
+        private Texture2D m_ScreenTexture;
+        private OutputDuplication m_DuplicatedOutput;
+        private int m_Width;
+        private int m_Height;
+
+
+        public static ScreenStateThread Instace { get { return m_Instance; } }
+        private static ScreenStateThread m_Instance = new ScreenStateThread();
+        private ScreenStateThread() { }
+
+        public void Init()
+        {
+            m_Init = true;
+
+            var adapter = new Factory1().GetAdapter1(0);
+            m_Device = new SharpDX.Direct3D11.Device(adapter);
+
+            var output = adapter.GetOutput(0);
+
+            m_Output1 = output.QueryInterface<Output1>();
+            m_Width = output.Description.DesktopBounds.Right;
+            m_Height = output.Description.DesktopBounds.Bottom;
+
+            m_DuplicatedOutput = m_Output1.DuplicateOutput(m_Device);
+
+            var textureDesc = new Texture2DDescription
+            {
+                CpuAccessFlags = CpuAccessFlags.Read,
+                BindFlags = BindFlags.None,
+                Format = Format.B8G8R8A8_UNorm,
+                Width = m_Width,
+                Height = m_Height,
+                OptionFlags = ResourceOptionFlags.None,
+                MipLevels = 1,
+                ArraySize = 1,
+                SampleDescription = { Count = 1, Quality = 0 },
+                Usage = ResourceUsage.Staging
+            };
+            m_ScreenTexture = new Texture2D(m_Device, textureDesc);
+
+            m_ScreenBmp = new Bitmap(m_Width, m_Height, PixelFormat.Format32bppArgb);
+            m_TempBitmap = new Bitmap(m_Width, m_Height, PixelFormat.Format32bppArgb);
+        }
+
+        public void Start()
+        {
+            if (!m_Init)
+                Init();
+
+            new StableRepeatingThread(() =>
+            {
+                TakeScreenshot(m_TempBitmap);
+
+                lock (ScreenBmpLock)
+                {
+                    BitmapUtility.Clone32BPPBitmap(m_TempBitmap, ScreenBmp);
+                }
+            }, 10).Start();
+        }
+
+        public void TakeScreenshot(Bitmap tempBitmap)
+        {
+            if (!m_Init)
+                Init();
+
+            try
+            {
+                SharpDX.DXGI.Resource screenResource;
+                OutputDuplicateFrameInformation duplicateFrameInformation;
+
+                // Try to get duplicated frame within given time is ms
+                m_DuplicatedOutput.AcquireNextFrame(20, out duplicateFrameInformation, out screenResource);
+
+                // copy resource into memory that can be accessed by the CPU
+                using (var screenTexture2D = screenResource.QueryInterface<Texture2D>())
+                    m_Device.ImmediateContext.CopyResource(screenTexture2D, m_ScreenTexture);
+
+                // Get the desktop capture texture
+                var mapSource = m_Device.ImmediateContext.MapSubresource(m_ScreenTexture, 0, MapMode.Read, MapFlags.None);
+
+                // using bitmap was here
+                var boundsRect = new Rectangle(0, 0, m_Width, m_Height);
+
+                // Copy pixels from screen capture Texture to GDI bitmap
+                var mapDest = tempBitmap.LockBits(boundsRect, ImageLockMode.WriteOnly, tempBitmap.PixelFormat);
+                var sourcePtr = mapSource.DataPointer;
+                var destPtr = mapDest.Scan0;
+                for (int y = 0; y < m_Height; y++)
+                {
+                    // Copy a single line 
+                    Utilities.CopyMemory(destPtr, sourcePtr, m_Width * 4);
+
+                    // Advance pointers
+                    sourcePtr = IntPtr.Add(sourcePtr, mapSource.RowPitch);
+                    destPtr = IntPtr.Add(destPtr, mapDest.Stride);
+                }
+
+                // Release source and dest locks
+                tempBitmap.UnlockBits(mapDest);
+
+                // Release memory
+                m_Device.ImmediateContext.UnmapSubresource(m_ScreenTexture, 0);
+                screenResource.Dispose();
+                m_DuplicatedOutput.ReleaseFrame();
+            }
+            catch (SharpDXException e)
+            {
+                if (e.ResultCode.Code != SharpDX.DXGI.ResultCode.WaitTimeout.Result.Code)
+                {
+                    Trace.TraceError(e.Message);
+                    Trace.TraceError(e.StackTrace);
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            m_Run = false;
+        }
+    }
+}
