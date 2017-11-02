0863e91c2be9a53ebb0d23ce62cfde0bbabf1a51
Justas <glodjus@gmail.com>
2017-11-02 01:39:35 +0100

    Adding new commands to move on image. No Run method currently

---
+++ RobotRuntime/Commands/CommandMoveOnImage.cs
@@ -0,0 +1,34 @@
+﻿using RobotRuntime.Utils.Win32;
+using System;
+using System.Drawing;
+
+namespace RobotRuntime.Commands
+{
+    [Serializable]
+    public class CommandMoveOnImage : Command
+    {
+        public Bitmap Image { get; set; }
+
+        public CommandMoveOnImage(Bitmap image)
+        {
+            Image = image;
+        }
+
+        public override object Clone()
+        {
+            return new CommandMoveOnImage(Image);
+        }
+
+        public override void Run()
+        {
+            int x1, y1;
+            x1 = WinAPI.GetCursorPosition().X;
+            y1 = WinAPI.GetCursorPosition().Y;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                // WinAPI.MouseMoveTo(x1 + ((X - x1) * i / 50), y1 + ((Y - y1) * i / 50));
+            }
+        }
+    }
+}
---
+++ RobotRuntime/Commands/CommandTeleportOnImage.cs
@@ -0,0 +1,27 @@
+﻿using RobotRuntime.Utils.Win32;
+using System;
+using System.Drawing;
+
+namespace RobotRuntime.Commands
+{
+    [Serializable]
+    public class CommandTeleportOnImage : Command
+    {
+        public Bitmap Image { get; set; }
+
+        public CommandTeleportOnImage(Bitmap image)
+        {
+            Image = image;
+        }
+
+        public override object Clone()
+        {
+            return new CommandTeleportOnImage(Image);
+        }
+
+        public override void Run()
+        {
+            // WinAPI.MouseMoveTo(X, Y);
+        }
+    }
+}
