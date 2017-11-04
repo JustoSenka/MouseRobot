16a28ff16fc775a3d90b4a818c7c62c325a05e87
Justas <glodjus@gmail.com>
2017-11-04 20:34:11 +0100

    Adding LimitedStack structure with tests

---
+++ RobotRuntime/Perf/LimitedStack.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RobotRuntime.Perf
+{
+    public class LimitedStack<T> : IEnumerable<T>
+    {
+        public int Limit { get; private set; }
+
+        private int m_First;
+        private int m_Last;
+        private T[] m_Data;
+
+        public LimitedStack(int limit)
+        {
+            Limit = limit;
+
+            m_First = -1;
+            m_Last = 0;
+            m_Data = new T[limit];
+        }
+
+        public void Add(T value)
+        {
+            m_First++;
+            if (m_First >= Limit)
+            {
+                m_First = 0;
+
+                if (m_First == m_Last)
+                    m_Last++;
+            }
+            else
+            {
+                if (m_First == m_Last && m_First != 0)
+                    m_Last++;
+            }
+
+            if (m_Last >= Limit)
+                m_Last = 0;
+
+            m_Data[m_First] = value;
+        }
+
+        public T Pop()
+        {
+            if (m_First == -1)
+                throw new InvalidOperationException("Stack is empty");
+
+            T ret = m_Data[m_First];
+            if (m_First == m_Last)
+            {
+                m_First = -1;
+                m_Last = 0;
+            }
+            else
+            {
+                m_First--;
+
+                if (m_First < 0)
+                    m_First = Limit - 1;
+            }
+
+            return ret;
+        }
+
+        public T this[int i]
+        {
+            get { return m_Data[i]; }
+            set { m_Data[i] = value; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (m_First == -1)
+                yield break;
+
+            if (m_First >= m_Last)
+            {
+                for (int i = m_First; i >= m_Last; --i)
+                    yield return m_Data[i];
+            }
+            else
+            {
+                for (int i = m_First; i >= 0; --i)
+                    yield return m_Data[i];
+
+                for (int i = Limit - 1; i >= m_Last; --i)
+                    yield return m_Data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Tests/LimitedStackTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using RobotRuntime.Perf;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class LimitedStackTests
+    {
+        [TestMethod]
+        public void LimitedStack_HasCorrectCount_InNonOverflowScenario()
+        {
+            var stack = new LimitedStack<int>(5);
+
+            stack.Add(1);
+            stack.Add(2);
+            stack.Add(3);
+
+            Assert.AreEqual(3, stack.Count());
+        }
+
+        [TestMethod]
+        public void LimitedStack_HasCorrectCount_InOverflowScenario()
+        {
+            var stack = new LimitedStack<int>(3);
+
+            stack.Add(1);
+            stack.Add(2);
+            stack.Add(3);
+            stack.Add(4);
+
+            Assert.AreEqual(3, stack.Count());
+        }
+
+        [TestMethod]
+        public void LimitedStack_HasCorrectValues_InOrder_NoOverflow()
+        {
+            var stack = new LimitedStack<int>(3);
+
+            stack.Add(1);
+            stack.Add(2);
+
+            var arr = stack.ToArray();
+
+            Assert.AreEqual(2, arr[0]);
+            Assert.AreEqual(1, arr[1]);
+        }
+
+        [TestMethod]
+        public void LimitedStack_HasCorrectValues_InOrder_AfterOverflow()
+        {
+            var stack = new LimitedStack<int>(3);
+
+            stack.Add(1);
+            stack.Add(2);
+            stack.Add(3);
+            stack.Add(4);
+            stack.Add(5);
+
+            var arr = stack.ToArray();
+
+            Assert.AreEqual(5, arr[0]);
+            Assert.AreEqual(4, arr[1]);
+            Assert.AreEqual(3, arr[2]);
+        }
+
+        [TestMethod]
+        public void LimitedStack_PopGivesBackCorrectValues_NoOverflow()
+        {
+            var stack = new LimitedStack<int>(4);
+
+            stack.Add(1);
+            stack.Add(2);
+            stack.Add(3);
+
+            Assert.AreEqual(3, stack.Pop());
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Count());
+
+            Assert.AreEqual(1, stack.Pop());
+            Assert.AreEqual(0, stack.Count());
+        }
+
+        [TestMethod]
+        public void LimitedStack_PopGivesBackCorrectValues_AfterOverflow()
+        {
+            var stack = new LimitedStack<int>(4);
+
+            stack.Add(1);
+            stack.Add(2);
+            stack.Add(3);
+            stack.Add(4);
+            stack.Add(5);
+            stack.Add(6);
+            stack.Add(7);
+
+            Assert.AreEqual(7, stack.Pop());
+            Assert.AreEqual(6, stack.Pop());
+            Assert.AreEqual(2, stack.Count());
+
+            Assert.AreEqual(5, stack.Pop());
+            Assert.AreEqual(4, stack.Pop());
+            Assert.AreEqual(0, stack.Count());
+        }
+
+        [TestMethod]
+        public void LimitedStack_AddingValuesAfterPopping_WithOverflow_StillGivesCorrectOrder()
+        {
+            var stack = new LimitedStack<int>(4);
+
+            stack.Add(1);
+            stack.Add(2);
+            stack.Pop();
+
+            stack.Add(3);
+            stack.Add(4);
+            stack.Pop();
+
+            stack.Add(5);
+            stack.Add(6);
+            stack.Pop();
+
+            stack.Add(7);
+            stack.Add(8);
+            stack.Pop();
+            stack.Add(9);
+
+            var arr = stack.ToArray();
+
+            Assert.AreEqual(9, arr[0]);
+            Assert.AreEqual(7, arr[1]);
+            Assert.AreEqual(5, arr[2]);
+            Assert.AreEqual(3, arr[3]);
+            Assert.AreEqual(4, arr.Length);
+        }
+
+        [TestMethod]
+        public void LimitedStack_PopingEmptyStack_Throws()
+        {
+            var stack = new LimitedStack<int>(4);
+
+            stack.Add(1);
+            stack.Pop();
+
+            bool gotException = false;
+            try
+            {
+                stack.Pop();
+            }
+            catch (Exception)
+            {
+                gotException = true;
+            }
+
+            Assert.IsTrue(gotException, "No exception if poping empty stack");
+        }
+    }
+}
