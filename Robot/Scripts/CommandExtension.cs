64eeb0666696e61bcda5103e2dfd1bfe73ee90a1
Justas <glodjus@gmail.com>
2018-06-18 16:31:59 +0300

    Revert "[WIP] Adding CommandCollection (subset of Script) and AbstractCommandCollectionManager (subset of ScriptManager)"
    This reverts commit 56f5e933bcb88e0c26b745331c4248a1dc19b092.

--- Robot/Scripts/CommandExtension.cs
+++ Robot/Scripts/CommandExtension.cs
@@ -5,21 +5,8 @@ namespace Robot
 {
     public static class CommandExtension
     {
-        public static int GetIndex(this Command command, AbstractCommandCollectionManager AbstractCommandCollectionManager)
-        {
-            if (AbstractCommandCollectionManager == null)
-                return 0;
-
-            var script = AbstractCommandCollectionManager.GetScriptFromCommand(command);
-            var node = script.Commands.GetNodeFromValue(command);
-            return node.parent.IndexOf(command);
-        }
-
         public static int GetIndex(this Command command, IScriptManager ScriptManager)
         {
-            if (ScriptManager == null)
-                return 0;
-
             var script = ScriptManager.GetScriptFromCommand(command);
             var node = script.Commands.GetNodeFromValue(command);
             return node.parent.IndexOf(command);
