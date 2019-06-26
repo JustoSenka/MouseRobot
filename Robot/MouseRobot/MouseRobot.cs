15259648fef2c7974ce53b2e772fb8366fd465d1
Justas <glodjus@gmail.com>
2019-06-26 22:34:12 +0200

    Reenabling new recording upon startup. Doesn't fail anymore for some reason

--- Robot/MouseRobot/MouseRobot.cs
+++ Robot/MouseRobot/MouseRobot.cs
@@ -62,7 +62,7 @@ namespace Robot
             this.InputCallbacks = InputCallbacks;
             this.StatusManager = StatusManager;
 
-            // HierarchyManager.NewRecording();
+            HierarchyManager.NewRecording();
             TestRunner.TestRunEnd += OnRecordingFinished;
         }
 
